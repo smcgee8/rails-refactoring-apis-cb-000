1439ec8df5a61d33bc31c81ed9e6d358ae0ef377
Sean McGee <sean.a.mcgee@gmail.com>
2018-02-10 19:51:43 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,6 @@ class SessionsController < ApplicationController
     github = GithubService.new
     github.authenticate!(ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_SECRET'], params[:code])
     session[:token] = github.access_token
-    raise session[:token].inspect
 
     session[:username] = github.get_username
 
