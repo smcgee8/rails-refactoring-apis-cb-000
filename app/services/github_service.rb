838fde241f6767563f9d0c67bcfef098b1f4c7b1
Sean McGee <sean.a.mcgee@gmail.com>
2018-02-10 20:03:43 +0000

    automatically backed up by learn

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -3,7 +3,7 @@ class GithubService
   attr_reader :access_token
 
   def initialize(access_hash = nil)
-    @access_token = access_hash[:access_token] if access_hash
+    @access_token = access_hash["access_token"] if access_hash
   end
 
   def authenticate!(client_id, client_secret, code)
