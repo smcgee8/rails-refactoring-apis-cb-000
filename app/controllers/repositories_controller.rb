fb4d7155c122ac9ab818cb35f3af6f063e3b4560
Sean McGee <sean.a.mcgee@gmail.com>
2018-02-10 19:52:42 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,12 +1,12 @@
 class RepositoriesController < ApplicationController
   def index
-    github = GithubService.new(session[:token])
+    github = GithubService.new({access_token: session[:token]})
     raise github.access_token.inspect
     @repos_array = github.get_repos
   end
 
   def create
-    github.GithubService.new(session[:token])
+    github.GithubService.new({access_token: session[:token]})
     github.create_repo(params[:name])
     redirect_to '/'
   end
