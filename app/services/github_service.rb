872a9b0070f1138be31fd111b9afaf57716c217b
Sean McGee <sean.a.mcgee@gmail.com>
2018-02-10 19:59:54 +0000

    automatically backed up by learn

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -23,7 +23,6 @@ class GithubService
   def get_repos
     response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
     repos_array = JSON.parse(response.body)
-    raise self.access_token.inspect
     repos_array.map{|repo| GithubRepo.new(repo) }
   end
 
