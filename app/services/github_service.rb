295fbfffd9da7ef0f6e3a215d912f18989d93556
Sean McGee <sean.a.mcgee@gmail.com>
2018-02-10 19:49:39 +0000

    automatically backed up by learn

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -11,6 +11,7 @@ class GithubService
         {client_id: client_id, client_secret: client_secret, code: code},
         {'Accept' => 'application/json'}
     access_hash = JSON.parse(response.body)
+    raise access_hash.inspect
     @access_token = access_hash["access_token"]
   end
 
